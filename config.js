b0bcfc376a4fb1b81afa8b232e645e6f3436169e
clearskyenergy <tom@clearsky-usa.com>
2026-07-07 16:49:27 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+window.CLEARSKY_CONFIG = {
+  firebase: {
+    apiKey: "AIzaSyABoM1lgOYUnd5ZadaoTMhYmA9cHa8Tyo0",
+    authDomain: "clearsky-portal.firebaseapp.com",
+    projectId: "clearsky-portal",
+    storageBucket: "clearsky-portal.firebasestorage.app",
+    messagingSenderId: "742134484347",
+    appId: "1:742134484347:web:ab0f95fd221536158481de"
+  },
+  googleMapsKey: "AIzaSyC0roZ_qMwtbVzqRLtNx9nrCeaGqrE1sNs",
+  allowedDomain: null,
+  companyName: "ClearSky USA",
+  portalTitle: "BESS Site Portal"
+};
